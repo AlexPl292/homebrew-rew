6ac1c3cc4aa03d3a6c5f2e284708fa12883c1b70
Alex Plate <AlexPl292@gmail.com>
2021-05-08 23:19:02 +0300

    Update sha

--- Formula/rew.rb
+++ Formula/rew.rb
@@ -4,7 +4,7 @@ class Rew < Formula
   desc "Keep your Brewfile up-to-date"
   homepage "https://github.com/AlexPl292/rew"
   url "https://github.com/AlexPl292/rew/releases/download/v0.1.0/rew.zip"
-  sha256 "c0d7d65fe61d5176b328cac6e2f18bfa195fbe6b3a3cf73e4b6895d5bea3d1e4"
+  sha256 "75cdd35df6e1b0b7895faaff5a80793d9a3885e31482a431e2441fe8997a67e5"
   version "0.1.0"
 
   depends_on "python@3.9"
